122e6b681e557bf98c51030b0e625cfb25507ec2
Emi <89019222+ENalb@users.noreply.github.com>
2023-01-23 23:09:03 +0400

    Add files via upload

---
+++ init/check2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+class Count {
+	int count = 0;
+    int order;
+public:
+	void foo()
+	{
+		++count;
+		order = count;
+		std::cout << "foo function -> " << order << std::endl;
+	}
+    Count() {
+        ++count;
+        order = count;
+        std::cout << "default ctor -> " << order << std::endl;
+    }
+};
+
+int main()
+{
+	Count obj;
+	obj.foo();
+}
